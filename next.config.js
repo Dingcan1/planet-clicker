a7fea5e673b792982159de005347df96ef744691
Dingcan1 <dingcanhn@gmail.com>
2025-09-18 20:59:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,47 +8,47 @@ const nextConfig = {
     return [
       {
         source: '/planet-clicker.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/planet-clicker/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/planet-clicker/index.html',
       },
       {
         source: '/bubble-shoot-merge-box-2048.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/bubble-shoot-merge-box-2048/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/bubble-shoot-merge-box-2048/index.html',
       },
       {
         source: '/crazy-cattle-3d.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/crazycattle3d/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/crazycattle3d/index.html',
       },
       {
         source: '/crazy-cow-3d.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/crazycow3d/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/crazycow3d/index.html',
       },
       {
         source: '/cheese-chompers-3d.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/cheesechompers3d/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/cheesechompers3d/index.html',
       },
       {
         source: '/brainrot-clicker.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/brainrot-clicker/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/brainrot-clicker/index.html',
       },
       {
         source: '/basketball-bros-unblocked.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/basketball-bros-unblocked/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/basketball-bros-unblocked/index.html',
       },
       {
         source: '/pokemon-gamma-emerald.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/pokemon-gamma-emerald/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/pokemon-gamma-emerald/index.html',
       },
       {
         source: '/crazy-chicken-3d.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/crazy-chicken-3d/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/crazy-chicken-3d/index.html',
       },
       {
         source: '/sprunki-incredibox.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/sprunki-incredibox/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/sprunki-incredibox/index.html',
       },
       {
         source: '/futbol-libre.embed',
-        destination: 'https://www.planetclickerbucket.dingcanhn.workers.dev/game/futbol-libre/index.html',
+        destination: 'https://planetclickerbucket.dingcanhn.workers.dev/game/futbol-libre/index.html',
       },
     ]
   }
